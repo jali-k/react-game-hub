b371c7aced7cd984662dfda74a9eec1a6995d6ea
jali-k <e19210@eng.pdn.ac.lk>
2023-09-02 16:33:52 +0530

    Create useGames hook to seperately fetch the games

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,27 +1,9 @@
 import { Grid, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GamesGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+  const { error, games } = useGames();
 
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
   return (
     <Grid>
       <>
