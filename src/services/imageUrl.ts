954e088f6070c4b54c1dc4286646b77a93064986
jali-k <e19210@eng.pdn.ac.lk>
2023-09-03 18:44:27 +0530

    Getting cropped images

---
+++ src/services/imageUrl.ts
@@ -0,0 +1,8 @@
+const getCroppedImageUrl = (url: String) => {
+    const target = 'media/'
+    const index = url.indexOf(target) + target.length;
+    let newUrl = url.slice(0, index)+ "crop/600/400/" + url.slice(index);
+    return newUrl;
+}
+
+export default getCroppedImageUrl;
